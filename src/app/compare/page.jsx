fb612bfe8fe0b025982d5d22ae3bca80417cb18a
Panda__Abhisek <abhisekpanda114@gmail.com>
2024-11-09 07:15:48 +0530

    add margitn top to compare btn

--- src/app/compare/page.jsx
+++ src/app/compare/page.jsx
@@ -121,7 +121,7 @@ export default function Component() {
           <Link href="/comparisonOne" passHref>
             <button
               type="submit"
-              className="w-full sm:w-auto bg-purple-700 text-white px-4 sm:px-6 py-2 rounded-md text-sm sm:text-base font-medium hover:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 transition-colors duration-200"
+              className="w-full sm:w-auto bg-purple-700 text-white px-4 sm:px-6 py-2 mt-2 rounded-md text-sm sm:text-base font-medium hover:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 transition-colors duration-200"
             >
               COMPARE
             </button>
